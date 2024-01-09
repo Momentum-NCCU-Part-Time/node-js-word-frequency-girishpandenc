25f8b441185ad5dd18b68c1b89089c1f629a12d9
Girish <gspande7@gmail.com>
2024-01-09 16:18:40 -0500

    formatting of output remaining

--- word_frequency.js
+++ word_frequency.js
@@ -38,11 +38,26 @@ function printWordFreq(file, callback) {
       process.exit(1)
     }
     // TODO: write code to count the words in the file
+    
+      const words = data.toLowerCase().split(/\W+/);
+      const wordFrequency = new Map();
+      for (let word of words) {
+        if (word === '') continue;
+        if (STOP_WORDS.includes(word)) continue;
+        // if (word = STOP_WORDS) continue;
+        if (wordFrequency.has(word)) {
+              wordFrequency.set(word, wordFrequency.get(word) + 1)
+        } else {
+          wordFrequency.set(word, 1)
+        }
+      }
+    
+    
     console.log('Initial data read from file: ', data)
-    callback('Your word count results should be passed in to this callback')
-  })
+    callback(wordFrequency)
+  });
 }
 
 printWordFreq(filePath, (wordCount) => {
   console.log('The results from your word counts:', wordCount)
-})
+});
